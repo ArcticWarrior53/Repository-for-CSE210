dd7cd7c146806853ddabf9457b941d4c4e6963c7
Jones Family <origamijedi05@gmail.com>
2023-01-07 23:01:24 -0500

    You can now quickly find your lettergrade using this program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string uinput = Console.ReadLine();
+        int grade = int.Parse(uinput);
+        string letter;
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your lettergrade is {letter}. ");
+        if (letter == "A" || letter == "B" || letter == "C")
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed... :(");
+        }
     }
 }
