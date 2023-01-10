810936b65c4e84a3672e8f31d261602909830f9d
Jones Family <origamijedi05@gmail.com>
2023-01-09 22:37:40 -0500

    I made it so that it will do all of the things it asks in the lesson. I would post a more detailed comment but I'm tired and I can't.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numlist = new List<int>();
+        int number;
+        int length = 0;
+        do
+        {
+            Console.Write("Enter number: ");
+            string StrNumber = Console.ReadLine();
+            number = int.Parse(StrNumber);
+            if (number != 0)
+            {
+                numlist.Add(number);
+                length++;
+            }
+            
+        } while(number != 0);
+        int sum = 0;
+        int largest = 0;
+        foreach (int num in numlist)
+        {
+            if (num > largest)
+            {
+                largest = num;
+            }
+            sum = num + sum; 
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        float average = sum/length;
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
