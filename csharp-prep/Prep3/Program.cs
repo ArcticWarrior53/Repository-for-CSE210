ba7e0c6a4ced06f4854cd530d2767399c8111f79
Jones Family <origamijedi05@gmail.com>
2023-01-09 21:01:22 -0500

    I created the if-if-else and loops, the random number generation, and the full guessing game nonsense!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //These collect input, the first prompt will be replaced later.
+        Random randomGenerator = new Random();
+        int mNum = randomGenerator.Next(1, 100);
+        int guess;
+        do
+        {
+            Console.Write("What is your guess? ");
+            string uinput = Console.ReadLine();
+            //This turns the input into numbers so I can work with them
+            guess = int.Parse(uinput);
+            //This checks the guess to see if it is higher, lower, or matches the magic number.
+            if (guess > mNum)
+            {  
+                Console.WriteLine("Lower");
+            }
+            else if (guess < mNum)
+            {  
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while(guess != mNum);
     }
 }
